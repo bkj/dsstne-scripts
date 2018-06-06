a84da4648f1178edb871ea44e02d087e514ff236
Ben Johnson <bkj.322@gmail.com>
2018-06-06 18:11:09 -0400

    testing

--- prep.py
+++ prep.py
@@ -1,37 +1,46 @@
-import pandas as pd
+#!/usr/bin/env python
+
+"""
+    prep.py
+"""
+
+from __future__ import print_function, division
+
+import os
+import sys
 import numpy as np
+import pandas as pd
 from sklearn.model_selection import train_test_split
-from tqdm import tqdm
 
-np.random.seed(567)
 
-edges = pd.read_csv('ml-20m/ratings.csv')
-del edges['timestamp']
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--inpath', type=str, default='data/ml-20m/ratings.csv')
+    parser.add_argument('--outpath', type=str, default='data')
+    parser.add_argument('--seed', type=int, default=123)
+    return parser.parse_args()
 
-train, test = train_test_split(edges, train_size=0.8, stratify=edges.userId)
 
-# # --
-# # Most frequent predictions
-
-# vcs   = train[1].value_counts()
-# top_k = pd.Series(vcs.head(10).index)
-# pk = pd.DataFrame(test).groupby(0)[1].apply(lambda x: top_k.isin(x))
-# pk.mean()
-
-# # 0.85
-# # ?? slides say it should be ~0.11 
-
-# # --
-
-train = train.sort_values(['userId', 'movieId']).reset_index(drop=True)
-train['movieRating'] = train.movieId.astype(str)# + ',' + train.rating.astype(str)
-train_feats = train.groupby('userId').movieRating.apply(lambda x: ':'.join(x))
-
-test  = test.sort_values(['userId', 'movieId']).reset_index(drop=True)
-test['movieRating'] = test.movieId.astype(str)# + ',' + test.rating.astype(str)
-test_feats  = test.groupby('userId').movieRating.apply(lambda x: ':'.join(x))
-
-assert train_feats.shape[0] == test_feats.shape[0]
-
-train_feats.to_csv('data/train.txt', sep='\t', header=None)
-test_feats.to_csv('data/test.txt', sep='\t', header=None)
+if __name__ == "__main__":
+    args = parse_args()
+    np.random.seed(args.seed)
+    
+    
+    print('loading %s' % args.inpath, file=sys.stderr)
+    edges = pd.read_csv(args.inpath)
+    del edges['timestamp']
+    
+    train, test = train_test_split(edges, train_size=0.8, stratify=edges.userId)
+    
+    train = train.sort_values(['userId', 'movieId']).reset_index(drop=True)
+    train['movieRating'] = train.movieId.astype(str)# + ',' + train.rating.astype(str)
+    train_feats = train.groupby('userId').movieRating.apply(lambda x: ':'.join(x))
+    
+    test  = test.sort_values(['userId', 'movieId']).reset_index(drop=True)
+    test['movieRating'] = test.movieId.astype(str)# + ',' + test.rating.astype(str)
+    test_feats  = test.groupby('userId').movieRating.apply(lambda x: ':'.join(x))
+    
+    assert train_feats.shape[0] == test_feats.shape[0]
+    
+    train_feats.to_csv(os.path.join(args.outpath, 'train.txt'), sep='\t', header=None)
+    test_feats.to_csv(os.path.join(args.outpath, 'test.txt'), sep='\t', header=None)
--- run.sh
+++ run.sh
@@ -7,26 +7,24 @@ mkdir -p {data,models,results}
 # --
 # IO
 
-unzip ml-20m.zip
+wget http://files.grouplens.org/datasets/movielens/ml-20m.zip
+unzip ml-20m.zip && mv ml-20m data && rm ml-20m.zip
 
-python prep.py
-
-export PATH="/home/bjohnson/software/amazon-dsstne/src/amazon/dsstne/bin/:$PATH"
-
-# Generate NetCDF files for input and output layers
-generateNetCDF -d gl_input -i data/train.txt -o data/gl_input.nc -f data/features_input -s data/samples_input -c
-generateNetCDF -d gl_output -i data/train.txt -o data/gl_output.nc -f data/features_output -s data/samples_input -c
+python prep.py --inpath data/ml-20m/ratings.csv --outpath data
 
 # --
 # Run
 
-CONFIG="config.json"
+export PATH="/home/bjohnson/software/amazon-dsstne/src/amazon/dsstne/bin/:$PATH"
+
+generateNetCDF -d gl_input -i data/train.txt -o data/gl_input.nc -f data/features_input -s data/samples_input -c
+generateNetCDF -d gl_output -i data/train.txt -o data/gl_output.nc -f data/features_output -s data/samples_input -c
 
 train -b 1024 -e 50 -n models/git_network.nc \
     -d gl \
     -i data/gl_input.nc \
     -o data/gl_output.nc \
-    -c $CONFIG 
+    -c config.json
 
 predict -b 2048 -k 10 -n models/git_network.nc \
     -d gl \
